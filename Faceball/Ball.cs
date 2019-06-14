fb52a6d30cac09ebb23525871a9f8b6f9bbbe4de
rosta45 <aleksandarpetrushev27@gmail.com>
2019-06-14 17:44:05 +0200

    Player Class done

---
+++ Faceball/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faceball
+{
+    public class Ball
+    {
+        public static int RADIUS = 15;
+        public Point Position { get; set; }
+        public int VelocityX { get; set; }
+        public int VelocityY { get; set; }
+        public 
+    }
+}
--- Faceball/Player.cs
+++ Faceball/Player.cs
@@ -1,12 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Faceball
 {
-	class Player
-	{
-	}
+    public enum DIRECTION
+    {
+        UP, UPRIGHT, RIGHT, DOWNRIGHT, DOWN, DOWNLEFT, LEFT, UPLEFT
+    }
+	public class Player
+    {
+        public static int MAX_VELOCITY = 20;
+        public static int Radius = 25;
+        public Point Position { get; set; }
+        public Icon Icon { get; set; }
+        public int VelocityX { get; set; }
+        public int VelocityY { get; set; }
+        public bool VodiTopka { get; set; }
+        public double Angle { get; set; }
+        public bool isMoving { get; set; }
+
+        public Player()
+        {
+        }
+
+        public void Move()
+        {
+            if (isMoving)
+            {
+                int newX = Position.X + VelocityX;
+                int newY = Position.Y + VelocityY;
+                Position = new Point(newX, newY);
+            }
+        }
+
+        public bool Shoot()
+        {
+            if (VodiTopka)
+            {
+                VodiTopka = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void DecreaseVelocity()
+        {
+            if (!isMoving)
+            {
+                VelocityX -= 3;
+                VelocityY -= 3;
+                if (VelocityX < 0)
+                    VelocityX = 0;
+                if (VelocityY < 0)
+                    VelocityY = 0;
+            }
+        }
+
+        public void IncreaseVelocity()
+        {
+            if (isMoving)
+            {
+                VelocityX += 3;
+                VelocityY += 3;
+                if (VelocityX > MAX_VELOCITY)
+                    VelocityX = MAX_VELOCITY;
+                if (VelocityY > MAX_VELOCITY)
+                    VelocityY = MAX_VELOCITY;
+            }
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawIcon(Icon, Position.X, Position.Y);
+        }
+    }
 }
