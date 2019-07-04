700a05d9c0a6ac0d77dcd5ec83ff44454e801f55
Risto Trajanov <risto.trajanov@gmail.com>
2019-07-04 14:49:43 +0200

    New Background

--- Faceball/Properties/Resources.Designer.cs
+++ Faceball/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Faceball.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FootballField {
+            get {
+                object obj = ResourceManager.GetObject("FootballField", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap gameBg {
             get {
                 object obj = ResourceManager.GetObject("gameBg", resourceCulture);
